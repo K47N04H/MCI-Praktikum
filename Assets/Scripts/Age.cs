defbfdb20c99e7943f746ac7ee43cb6cc05ed6c3
LiesaE <liesa.engel@studium.uni-hamburg.de>
2021-09-29 15:03:22 +0200

    Age

---
+++ Assets/Scripts/Age.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Age : MonoBehaviour
+{
+    public GameObject femaleFitYoung;
+    public GameObject femaleSlimYoung;
+    public GameObject femaleChubbyYoung;
+    public GameObject femaleFitOld;
+    public GameObject femaleSlimOld;
+    public GameObject femaleChubbyOld;
+
+    
+
+    public void ChangeAgeForward()
+    {
+        if(femaleSlimYoung.activeInHierarchy == true)
+        {
+
+                femaleFitOld.SetActive(false);
+                femaleSlimOld.SetActive(true);
+                femaleChubbyOld.SetActive(false);
+                femaleSlimYoung.SetActive(false);
+
+
+
+        }
+
+        else if(femaleChubbyYoung.activeInHierarchy == true)
+        {
+                femaleFitOld.SetActive(false);
+                femaleSlimOld.SetActive(false);
+                femaleChubbyOld.SetActive(true);
+                femaleChubbyYoung.SetActive(false);
+
+
+        }
+
+        else if (femaleFitYoung.activeInHierarchy == true)
+        {
+
+                femaleFitOld.SetActive(true);
+                femaleSlimOld.SetActive(false);
+                femaleChubbyOld.SetActive(false);
+                femaleFitYoung.SetActive(false);
+
+        }
+
+    }
+}
--- Assets/Scripts/ChangeCategorie.cs
+++ Assets/Scripts/ChangeCategorie.cs
@@ -11,25 +11,143 @@ public class ChangeCategorie : MonoBehaviour
     private int numberCategory = 0;
 
 
-
     // Change categorie forward while clicking the right button
     public void ChangeCategorieForward()
     {
-        textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[numberCategory];
-        if(numberCategory <= categories.Length)
+        if (numberCategory == 0)
         {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[0];
             numberCategory += 1;
-        }     
-}
+        }
 
-    // Change categorie back while clicking the left button
+        else if (numberCategory == 1)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[1];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 2)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[2];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 3)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[3];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 4)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[4];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 5)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[5];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 6)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[6];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 7)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[7];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 8)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[8];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 9)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[9];
+            numberCategory += 1;
+        }
+
+        else if (numberCategory == 10)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[10];
+            numberCategory += 1;
+        }
+
+    }
+
     public void ChangeCategorieBack()
     {
-        textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[numberCategory];
-        if(numberCategory >= 0)
+        if (numberCategory == 0)
         {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[0];
             numberCategory -= 1;
         }
 
+        else if (numberCategory == 1)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[1];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 2)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[2];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 3)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[3];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 4)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[4];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 5)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[5];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 6)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[6];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 7)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[7];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 8)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[8];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 9)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[9];
+            numberCategory -= 1;
+        }
+
+        else if (numberCategory == 10)
+        {
+            textCategorie.GetComponent<TMPro.TMP_Text>().text = categories[10];
+            numberCategory -= 1;
+        }
     }
 }
